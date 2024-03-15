2b13fd2c99a7779dd1c5fa8b1689a93e0bfa60c4
opezdalya <opezdalya@gmail.com>
2024-03-15 13:20:28 +0000

    Commit

--- SM2SignatureAlgorithm/EC.cpp
+++ SM2SignatureAlgorithm/EC.cpp
@@ -1,4 +1,4 @@
-#include "EC.h"
+﻿#include "EC.h"
 
 EC::CommonParameters EC::getCommonParameters()
 {
@@ -12,4 +12,47 @@ EC::CommonParameters EC::getCommonParameters()
     params.basePoint = findBasePoint(params.curveParams);
 
     return params;
+}
+
+EC::GF_p EC::scalarMultiplication(const GF_p& point, int k)
+{
+    GF_p result;
+    result.x = point.x;
+    result.y = point.y;
+
+    for (int i = 1; i < k; ++i) {
+        result = ECAddition(result, point);
+    }
+
+    return result;
+}
+
+EC::GF_p EC::ECAddition(const GF_p& point1, const GF_p& point2)
+{
+    GF_p result;
+
+    if (point1.x == point2.x && point1.y != point2.y) {
+        result.x = 0;
+        result.y = 0;
+        return result; // Операция неопределена - точки на разных касательных
+    }
+
+    int lambda;
+    if (point1 == point2) { // Если точки равны, используем производную
+        lambda = (3 * point1.x * point1.x + this->ecParams.a) * pow(2 * point1.y, -1);
+    }
+    else {
+        lambda = (point2.y - point1.y) * pow(point2.x - point1.x, -1);
+    }
+
+    result.x = (lambda * lambda - point1.x - point2.x) % this->ecParams.p;
+    result.y = (lambda * (point1.x - result.x) - point1.y) % this->ecParams.p;
+
+    return result;
+}
+
+
+std::string EC::generateKey(CommonParameters getCommonParameters)
+{
+    return std::string();
 }
--- SM2SignatureAlgorithm/EC.h
+++ SM2SignatureAlgorithm/EC.h
@@ -8,6 +8,10 @@ class EC
 private:
     struct GF_p {
         int x, y;
+        // Define the equality operator
+        bool operator==(const GF_p& other) const {
+            return x == other.x && y == other.y;
+        }
     };
     struct ECParams {
 		int x, a, b, p;
@@ -36,16 +40,18 @@ public:
 	function ECMul()
 	constructor EC
 	*/
-    EC(const ECParams& ecParams) {
+    EC(const ECParams& ecParams){
         this->ecParams = ecParams;
     }
+
     struct CommonParameters {
         ECParams curveParams; // Eliptic Curve Parameters
         GF_p basePoint; // Base point
     };
 
     CommonParameters getCommonParameters();
-
+    GF_p scalarMultiplication(const GF_p& point, int k);
+    GF_p ECAddition(const GF_p& point1, const GF_p& point2);
     std::string generateKey(CommonParameters getCommonParameters);
 };
 
